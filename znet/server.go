cb7cd15258455044ebde7ce3e55585c48f2e85b6
Aioloscn <756243011@qq.com>
2022-03-07 00:23:26 +0800

    Initialize the project

---
+++ znet/server.go
@@ -0,0 +1,88 @@
+package znet
+
+import (
+	"fmt"
+	"net"
+	"zinx/ziface"
+)
+
+type Server struct {
+	// 服务器的名称
+	Name string
+	// 服务器绑定的ip版本
+	IPVersion string
+	// 服务器监听的IP
+	IP string
+	// 服务器监听的端口
+	Port int
+}
+
+// Start 启动服务器
+func (s *Server) Start() {
+	fmt.Printf("[Start] Server Listener at IP: %s, Port %d, is starting\n", s.IP, s.Port)
+
+	go func() {
+		// 1. 获取一个TCP的Addr
+		addr, err := net.ResolveTCPAddr(s.IPVersion, fmt.Sprintf("%s:%d", s.IP, s.Port))
+		if err != nil {
+			fmt.Println("resolve tcp addr error: ", err)
+			return
+		}
+
+		// 2. 监听服务器的地址
+		listener, err := net.ListenTCP(s.IPVersion, addr)
+		if err != nil {
+			fmt.Println("listen ", s.IPVersion, "error: ", err)
+			return
+		}
+		fmt.Println("start Zinx server ", s.Name, "success, listening")
+
+		// 3. 阻塞等待客户端连接，处理客户端连接业务（读写）
+		for {
+			// 如果有客户端连接过来，阻塞会返回
+			conn, err := listener.AcceptTCP()
+			if err != nil {
+				fmt.Println("Accept err: ", err)
+				continue
+			}
+			// 已经和客户端建立连接，做一个最基本的最大512字节的回显业务
+			go func() {
+				for {
+					buf := make([]byte, 512)
+					cnt, err := conn.Read(buf)
+					if err != nil {
+						fmt.Println("receive buf err", err)
+						continue
+					}
+					// 回显
+					if _, err := conn.Write(buf[:cnt]); err != nil {
+						fmt.Println("write back buf err", err)
+						continue
+					}
+				}
+			}()
+		}
+	}()
+}
+
+// Stop 停止服务器
+func (s *Server) Stop() {
+
+}
+
+// Serve 运行服务器
+func (s *Server) Serve() {
+	s.Start()
+
+	select {}
+}
+
+func NewServer(name string) ziface.IServer {
+	s := &Server{
+		Name:      name,
+		IPVersion: "tcp4",
+		IP:        "0.0.0.0",
+		Port:      8999,
+	}
+	return s
+}
